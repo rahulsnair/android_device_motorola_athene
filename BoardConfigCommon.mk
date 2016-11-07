38d583070af9fe8f0af9b46c1e3714c80cc49b74
Silesh K Nair <sileshn@gmail.com>
2016-11-07 18:53:44 +0530

    Use hardware/cyanogen VibratorHW implementation

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,9 +79,7 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 
 # CMHW
 BOARD_USES_CYANOGEN_HARDWARE := true
-BOARD_HARDWARE_CLASS += \
-    $(VENDOR_PATH)/cmhw \
-    hardware/cyanogen/cmhw
+BOARD_HARDWARE_CLASS += hardware/cyanogen/cmhw
 
 # Display
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
