9bee94e0bb98d9381b72ab4eb9596ea0793ba68e
David Marble <drmarble1@gmail.com>
2016-11-12 14:25:46 -0600

    TARGET_BOOTANIMATION_HALF_RES to speed up boots
    Boot animation uses ~140% cpu (out of 400%).
    This decreases it to ~52% so boot time is cut.
    This especially helps on first boot because
    dex2oat is faster.
    
    Change-Id: Ic965ae76e5656a98edfbfe869fcb843ff18296b4

--- cm.mk
+++ cm.mk
@@ -20,6 +20,7 @@ $(call inherit-product, device/motorola/athene/full_athene.mk)
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
+TARGET_BOOTANIMATION_HALF_RES := true
 
 # OTA
 PRODUCT_PROPERTY_OVERRIDES += \
