ce9a140da0b60755256031a6497a34275d4a4ee8
Alberto97 <albertop2197@gmail.com>
2016-12-26 18:25:22 +0100

    Drop irqbalance
    * It does nothing but spamming

--- device.mk
+++ device.mk
@@ -148,10 +148,6 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
-# IRQ
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
-
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
