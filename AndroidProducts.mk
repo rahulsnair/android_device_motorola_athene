1a174fb14ed939cc3c9169e8d85747839ef9253e
rahulsnair <rahulsnair30@gmail.com>
2017-01-11 14:11:07 +0000

    athene : prepare for nitrogen

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2015 The CyanogenMod Project
-#           (C) 2017 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,17 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-# Get the long list of APNs
-PRODUCT_COPY_FILES := device/motorola/athene/apns-full-conf.xml:system/etc/apns-conf.xml
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/nitrogen_athene.mk
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
-# Inherit from athene device
-$(call inherit-product, device/motorola/athene/device.mk)
-
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := athene
-PRODUCT_NAME := full_athene
